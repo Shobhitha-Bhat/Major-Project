1e077599687d47f359b80abf663188e4a7917832
Shobhitha Bhat <sbbhat2004@gmail.com>
2025-09-26 11:57:22 +0530

    modifications'

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ import mongoose from "mongoose";
 import cors from "cors"
 
 
-const port = 8000
+
 const app = express() //fetch express functionality
 app.use(cors())
 app.use(express.json())
 
+const port = process.env.PORT || 8000;
 
-mongoose.connect('mongodb+srv://shobhitha_bhat:shobiacon2208@cluster0.uugyuwo.mongodb.net/LORA')
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
     console.log("Database Connected")
 })
